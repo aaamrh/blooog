c01ec4a3f8204de1942a8c15009a3d525d3fa451
maruihua <aaamrh@163.com>
2022-05-19 20:59:13 +0800

    feat: redis

--- back/src/cache/_redis.js
+++ back/src/cache/_redis.js
@@ -1,27 +1,22 @@
 const redis = require('redis')
 const { REDIS_CONF } = require('../conf/db')
 
-
-// const redisClient = redis.createClient()
 const redisClient = redis.createClient({
   socket: {
     host: REDIS_CONF.host,
     port: REDIS_CONF.port,
   },
-  legacyMode: true,
+  // legacyMode: true,
 })
 
 redisClient.on('error', err => {
   console.error('redis error', err)
 })
 
-async function connect() {
+;(async () => {
   await redisClient.connect();
+})();
 
-  const value = await redisClient.get('name')
-}
-
-connect()
 
 async function rset (key, val, timeout = 60 * 60) {
   if (typeof val === 'object') {
@@ -29,41 +24,13 @@ async function rset (key, val, timeout = 60 * 60) {
   }
 
   await redisClient.set(key, val)
-  redisClient.expire(key, timeout)
+  await redisClient.expire(key, timeout)
 }
 
 async function rget (key) {
   const value = await redisClient.get(key)
-  console.log('value', value)
   return value
 }
-
-function get(key) {
-  const promise = new Promise((resolve, reject) => {
-    redisClient.get(key, (err, val) => {
-      if (err) {
-        reject(err)
-        return
-      }
-      if (val == null) {
-        resolve(null)
-        return
-      }
-
-      try {
-        resolve(
-          JSON.parse(val)
-        )
-      } catch (ex) {
-        resolve(val)
-      }
-    })
-  })
-  return promise
-}
-
-rset('name1', '3333')
-rget('name1')
 
 module.exports = {
   rset,
